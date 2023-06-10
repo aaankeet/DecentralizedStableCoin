ac85821ee6a55dc2fd9e5ac9dc4b65be7c9f8a8b
Ankit <thatboyankit@gmail.com>
2023-06-10 23:24:14 +0530

    modified Invariants.t.sol

--- test/fuzz/Invariants.t.sol
+++ test/fuzz/Invariants.t.sol
@@ -7,7 +7,7 @@
 
 pragma solidity 0.8.18;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
 
 import {DeployDsc} from "../../script/DeployDsc.s.sol";
@@ -16,14 +16,17 @@ import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 
+import {Handler} from "./Handler.t.sol";
+
 import {IERC20} from "@oz/token/ERC20/IERC20.sol";
 
 contract InvariantTest is StdInvariant, Test {
     DeployDsc deployer;
     DSCEngine dscEngine;
     DecentralizedStableCoin dsc;
-
     HelperConfig config;
+
+    Handler handler;
 
     address public wEthPriceFeed;
     address public wBtcPriceFeed;
@@ -35,7 +38,9 @@ contract InvariantTest is StdInvariant, Test {
         deployer = new DeployDsc();
         (dsc, dscEngine, config) = deployer.run();
 
-        targetContract(address(dscEngine));
+        handler = new Handler(dscEngine, dsc);
+        targetContract(address(handler));
+
         (wEthPriceFeed, wBtcPriceFeed, wEth, wBtc,) = config.activeNetworkConfig();
     }
 
@@ -43,14 +48,19 @@ contract InvariantTest is StdInvariant, Test {
         // get the value of all the collateral in the protocol;
         // compare it to all  the debt (dsc)
 
-        uint256 totalSupply = dsc.totalSupply();
-
         uint256 totalWethDeposited = IERC20(wEth).balanceOf(address(dscEngine));
         uint256 totalBtcDeposited = IERC20(wBtc).balanceOf(address(dscEngine));
 
-        uint256 totalWethValue = dscEngine.getValueInUSD(wEth, totalWethDeposited);
+        uint256 totalEthValue = dscEngine.getValueInUSD(wEth, totalWethDeposited);
         uint256 totalBtcValue = dscEngine.getValueInUSD(wBtc, totalBtcDeposited);
 
-        assert(totalBtcValue + totalWethValue >= totalSupply);
+        uint256 totalSupply = dsc.totalSupply();
+
+        console.log("Total Supply:", totalSupply / 1e18);
+        console.log("Total Eth Value:", totalEthValue / 1e18);
+        console.log("Total Btc Value:", totalBtcValue / 1e18);
+        console.log("Times Mint Called:", handler.timeMintCalled());
+
+        assert(totalBtcValue + totalEthValue >= totalSupply);
     }
 }
