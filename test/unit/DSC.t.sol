bf10f76dd9ae5f80d6554ab5c2fb379a3ef89cdd
Ankit <thatboyankit@gmail.com>
2023-06-12 23:22:13 +0530

    DSC Test 100% Coverage

--- test/unit/DSC.t.sol
+++ test/unit/DSC.t.sol
@@ -7,6 +7,7 @@ import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 
 error DecentralizedStableCoin_AmountMustBeAboveZero();
 error DecentralizedStableCoin_InvalidAddress();
+error DecentralizedStableCoin_AmountExceedsBalance();
 
 contract DSCTest is Test {
     DecentralizedStableCoin dsc;
@@ -17,8 +18,23 @@ contract DSCTest is Test {
         dsc = new DecentralizedStableCoin();
     }
 
-    function testRevertIfAmountIsZero() public {
+    function testRevertMintIfAmountIsZero() public {
         vm.expectRevert(DecentralizedStableCoin_AmountMustBeAboveZero.selector);
         dsc.mint(ALICE, 0);
+    }
+
+    function testRevertMintIfAddressIfZero() public {
+        vm.expectRevert(DecentralizedStableCoin_InvalidAddress.selector);
+        dsc.mint(address(0), AMOUNT);
+    }
+
+    function testRevertBurnIfAmountIsZero() public {
+        vm.expectRevert(DecentralizedStableCoin_AmountMustBeAboveZero.selector);
+        dsc.burn(0);
+    }
+
+    function testRevertBalanceIsLessThanAmount() public {
+        vm.expectRevert(DecentralizedStableCoin_AmountExceedsBalance.selector);
+        dsc.burn(AMOUNT);
     }
 }
